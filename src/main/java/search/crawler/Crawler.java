9beb282886505ee48ddef2e9e99177389f875ff8
rohitsinghatwadia <rohitsinghatwadia@gmail.com>
2023-09-27 00:56:46 +0530

    searchBase

---
+++ src/main/java/search/crawler/Crawler.java
@@ -0,0 +1,11 @@
+package search.crawler;
+
+import search.url.URL;
+import java.util.List;
+
+public class Crawler {
+
+    public List<URL> get(String query){
+        return null;
+    }
+}
---
+++ src/main/java/search/searchAPI.java
@@ -0,0 +1,15 @@
+package search;
+
+import search.url.URL;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class searchAPI {
+    //Think about multithreading later
+    public static List<URL> search(String query ){
+        List<URL> searchResult = new ArrayList<>();
+
+        return searchResult;
+    }
+}
